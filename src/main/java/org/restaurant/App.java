b509fa298907ec2a398c56d03a78cb2a2c036efb
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-22 19:00:08 +0100

    feat: Add sort of restaurants by name and rating

--- src/main/java/org/restaurant/App.java
+++ src/main/java/org/restaurant/App.java
@@ -1,17 +1,22 @@
 package org.restaurant;
 
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class App
 {
     public static void main( String[] args )
     {
-        HashSet<Restaurant> restaurants = new HashSet<>();
+//      HashSet<Restaurant> restaurants = new HashSet<>();
+        TreeSet<Restaurant> restaurants = new TreeSet<>();
 
         restaurants.add(new Restaurant("Italian Bistro", 8));
         restaurants.add(new Restaurant("Italian Bistro", 9));
         restaurants.add(new Restaurant("Sushi Zen", 7));
         restaurants.add(new Restaurant("Italian Bistro", 8));
+        restaurants.add(new Restaurant("Sushi Zen", 10));
+        restaurants.add(new Restaurant("Amàlia", 5));
 
         System.out.println("--- Restaurants in HashSet ---");
 
