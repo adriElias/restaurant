d59dc75ddc1e3ac8af4556198f238a0e1ec906c1
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-22 16:50:25 +0100

    feat: create Restaurant class with name and rating

---
+++ src/main/java/org/restaurant/Restaurant.java
@@ -0,0 +1,32 @@
+package org.restaurant;
+
+public class Restaurant {
+    private String name;
+    private int rating;
+
+    public Restaurant(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurants: \n" + "name: '" + name + "(" + rating + ")";
+    }
+}
